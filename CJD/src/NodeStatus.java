8e8129f5853c035c67b63f4300c5a01afdee5813
unknown <happier.yiyaoliu@gmail.com>
2012-11-01 19:31:43 -0400

    initial try, receive UDP package (copied) and NodeStatus prototype

---
+++ CJD/src/NodeStatus.java
@@ -0,0 +1,21 @@
+
+public class NodeStatus {
+	float cpuUsage;
+	float memoryUsage;
+	float bandwidth;
+	
+	public void AddIntoQueue()
+	{
+		
+	}
+	
+	public void UpdateCurrentNode()
+	{
+		
+	}
+	
+	public void DeleteCurrentNode()
+	{
+		
+	}
+}
