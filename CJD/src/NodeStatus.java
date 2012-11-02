79421ad5d9b444dc04b763fd5f24a82764ebc3d8
unknown <happier.yiyaoliu@gmail.com>
2012-11-01 20:05:43 -0400

    good night :)

--- CJD/src/NodeStatus.java
+++ CJD/src/NodeStatus.java
@@ -1,19 +1,38 @@
 
+//the info that needs to be recorded for each node server 
+
 public class NodeStatus {
-	float cpuUsage;
-	float memoryUsage;
-	float bandwidth;
+	//unique identifier for each node
+	private int id; 
+	
+	//info of the node server
+	private float cpuUsage;
+	private float memoryUsage;
+	private float bandwidth;
+	
+	//map that 
+	
+	public NodeStatus(int newId, float newCpuUsage, float newMemoryUsage, float newBandwidth)
+	{
+		id = newId;
+		cpuUsage = newCpuUsage;
+		memoryUsage = newMemoryUsage;
+		bandwidth = newBandwidth;
+	}
 	
 	public void AddIntoQueue()
 	{
 		
 	}
 	
-	public void UpdateCurrentNode()
+	public void UpdateCurrentNode(float updatedCpuUsage, float updatedMemoryUsage, float updatedBandwidth)
 	{
-		
+		cpuUsage = updatedCpuUsage;
+		memoryUsage = updatedMemoryUsage;
+		bandwidth = updatedBandwidth;
 	}
-	
+
+	// maybe we don't need this, LOL 
 	public void DeleteCurrentNode()
 	{
 		
