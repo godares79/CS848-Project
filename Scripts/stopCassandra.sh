a68f4c15816f580504f163037128c2f62d269dfc
David <david@david-server.(none)>
2012-11-14 01:34:09 -0500

    stopping and starting lcuster

---
+++ Scripts/stopCassandra.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# Will shutdown cassandra on all nodes
+# Assumes that the cassandra process pid handle is 'java' which isn't very nice,
+# but necessary
+# Also assumes that you have already stopped the cassandra process on ugster02
+# (at least, it doesn't shut it down for you)
+
+echo "STOPPING CASSANDRA NODES (EXCEPT ugster02)"
+
+`ssh ugster01 'kill $(pidof java)'`
+`ssh ugster03 'kill $(pidof java)'`
+`ssh ugster04 'kill $(pidof java)'`
+`ssh ugster06 'kill $(pidof java)'`
+`ssh ugster07 'kill $(pidof java)'`
+`ssh ugster08 'kill $(pidof java)'`
+`ssh ugster09 'kill $(pidof java)'`
+`ssh ugster10 'kill $(pidof java)'`
+`ssh ugster11 'kill $(pidof java)'`
+
