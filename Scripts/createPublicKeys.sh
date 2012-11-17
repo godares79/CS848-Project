86c7bdfffb9d564bbd22f820d7eb9b8e84c87227
David <david@david-server.(none)>
2012-11-17 01:56:05 -0500

    more changes

--- Scripts/createPublicKeys.sh
+++ Scripts/createPublicKeys.sh
@@ -7,23 +7,23 @@ echo "CREATING SSH KEY (USE AN EMPTY PASSPHRASE)"
 echo "ENTER SSH USERNAME:"
 read usrName
 
-`ssh ugster01 'mkdir /home/$usrName/.ssh'`
+`ssh ugster01 "mkdir /home/$usrName/.ssh"`
 `rsync -ave ssh .ssh/id_dsa.pub $usrName@ugster01:/home/$usrName/.ssh/authorized_keys`
-`ssh ugster03 'mkdir /home/$usrName/.ssh'`
+`ssh ugster03 "mkdir /home/$usrName/.ssh"`
 `rsync -ave ssh .ssh/id_dsa.pub $usrName@ugster03:/home/$usrName/.ssh/authorized_keys`
-`ssh ugster04 'mkdir /home/$usrName/.ssh'`
+`ssh ugster04 "mkdir /home/$usrName/.ssh"`
 `rsync -ave ssh .ssh/id_dsa.pub $usrName@ugster04:/home/$usrName/.ssh/authorized_keys`
-`ssh ugster06 'mkdir /home/$usrName/.ssh'`
+`ssh ugster06 "mkdir /home/$usrName/.ssh"`
 `rsync -ave ssh .ssh/id_dsa.pub $usrName@ugster06:/home/$usrName/.ssh/authorized_keys`
-`ssh ugster07 'mkdir /home/$usrName/.ssh'`
+`ssh ugster07 "mkdir /home/$usrName/.ssh"`
 `rsync -ave ssh .ssh/id_dsa.pub $usrName@ugster07:/home/$usrName/.ssh/authorized_keys`
-`ssh ugster08 'mkdir /home/$usrName/.ssh'`
+`ssh ugster08 "mkdir /home/$usrName/.ssh"`
 `rsync -ave ssh .ssh/id_dsa.pub $usrName@ugster08:/home/$usrName/.ssh/authorized_keys`
-`ssh ugster09 'mkdir /home/$usrName/.ssh'`
+`ssh ugster09 "mkdir /home/$usrName/.ssh"`
 `rsync -ave ssh .ssh/id_dsa.pub $usrName@ugster09:/home/$usrName/.ssh/authorized_keys`
-`ssh ugster10 'mkdir /home/$usrName/.ssh'`
+`ssh ugster10 "mkdir /home/$usrName/.ssh"`
 `rsync -ave ssh .ssh/id_dsa.pub $usrName@ugster10:/home/$usrName/.ssh/authorized_keys`
-`ssh ugster11 'mkdir /home/$usrName/.ssh'`
+`ssh ugster11 "mkdir /home/$usrName/.ssh"`
 `rsync -ave ssh .ssh/id_dsa.pub $usrName@ugster11:/home/$usrName/.ssh/authorized_keys`
 
 
