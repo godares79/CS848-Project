8237d1827172dce0e0c4671d6e6e10a92bc39203
David Dietrich <daviddietrich@ddietrich-lt.cs.uwaterloo.ca>
2012-11-21 00:00:18 -0500

    changed the stop cassandra script

--- Scripts/stopCassandra.sh
+++ Scripts/stopCassandra.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash -x
 # Will shutdown cassandra on all nodes
 # Assumes that the cassandra process pid handle is 'java' which isn't very nice,
 # but necessary
@@ -6,15 +6,42 @@
 # (at least, it doesn't shut it down for you)
 
 echo "STOPPING CASSANDRA NODES (EXCEPT ugster02)"
-echo "SEE THE stop-cassandra SCRIPT IN THE CASSANDRA BIN FOR A BETTER APPROACH"
 
-`ssh ugster01 'kill $(pidof java)'`
-`ssh ugster03 'kill $(pidof java)'`
-`ssh ugster04 'kill $(pidof java)'`
-`ssh ugster06 'kill $(pidof java)'`
-`ssh ugster07 'kill $(pidof java)'`
-`ssh ugster08 'kill $(pidof java)'`
-`ssh ugster09 'kill $(pidof java)'`
-`ssh ugster10 'kill $(pidof java)'`
-`ssh ugster11 'kill $(pidof java)'`
+`ssh ugster01 "pgrep -u \`whoami\` -f cassandra | xargs kill -9"`
+`ssh ugster03 "pgrep -u \`whoami\` -f cassandra | xargs kill -9"`
+`ssh ugster04 "pgrep -u \`whoami\` -f cassandra | xargs kill -9"`
+`ssh ugster06 "pgrep -u \`whoami\` -f cassandra | xargs kill -9"`
+`ssh ugster07 "pgrep -u \`whoami\` -f cassandra | xargs kill -9"`
+`ssh ugster08 "pgrep -u \`whoami\` -f cassandra | xargs kill -9"`
+`ssh ugster09 "pgrep -u \`whoami\` -f cassandra | xargs kill -9"`
+`ssh ugster10 "pgrep -u \`whoami\` -f cassandra | xargs kill -9"`
+`ssh ugster11 "pgrep -u \`whoami\` -f cassandra | xargs kill -9"`
 
+echo "STOPPING RESOURCE MONITOR NODES (and rmiregistry)"
+
+`ssh ugster01 "pgrep -u \`whoami\` -f ResourceMonitor | xargs kill -9"`
+`ssh ugster02 "pgrep -u \`whoami\` -f ResourceMonitor | xargs kill -9"`
+`ssh ugster03 "pgrep -u \`whoami\` -f ResourceMonitor | xargs kill -9"`
+`ssh ugster04 "pgrep -u \`whoami\` -f ResourceMonitor | xargs kill -9"`
+`ssh ugster06 "pgrep -u \`whoami\` -f ResourceMonitor | xargs kill -9"`
+`ssh ugster07 "pgrep -u \`whoami\` -f ResourceMonitor | xargs kill -9"`
+`ssh ugster08 "pgrep -u \`whoami\` -f ResourceMonitor | xargs kill -9"`
+`ssh ugster09 "pgrep -u \`whoami\` -f ResourceMonitor | xargs kill -9"`
+`ssh ugster10 "pgrep -u \`whoami\` -f ResourceMonitor | xargs kill -9"`
+`ssh ugster11 "pgrep -u \`whoami\` -f ResourceMonitor | xargs kill -9"`
+`ssh ugster01 "pgrep -u \`whoami\` -f rmiregistry | xargs kill -9"`
+`ssh ugster02 "pgrep -u \`whoami\` -f rmiregistry | xargs kill -9"`
+`ssh ugster03 "pgrep -u \`whoami\` -f rmiregistry | xargs kill -9"`
+`ssh ugster04 "pgrep -u \`whoami\` -f rmiregistry | xargs kill -9"`
+`ssh ugster06 "pgrep -u \`whoami\` -f rmiregistry | xargs kill -9"`
+`ssh ugster07 "pgrep -u \`whoami\` -f rmiregistry | xargs kill -9"`
+`ssh ugster08 "pgrep -u \`whoami\` -f rmiregistry | xargs kill -9"`
+`ssh ugster09 "pgrep -u \`whoami\` -f rmiregistry | xargs kill -9"`
+`ssh ugster10 "pgrep -u \`whoami\` -f rmiregistry | xargs kill -9"`
+`ssh ugster11 "pgrep -u \`whoami\` -f rmiregistry | xargs kill -9"`
+
+echo "STOPPING CJD ON UGSTER02"
+`ssh ugster02 "pgrep -u \`whoami\` -f CJD | xargs kill -9"`
+
+echo "STOPPING CASSANDRA ON UGSTER02"
+`ssh ugster02 "pgrep -u \`whoami\` -f cassandra | xargs kill -9"`
