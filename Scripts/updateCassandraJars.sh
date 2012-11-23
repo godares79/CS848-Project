a399b740b4ca06cde03f9cbeff943c7d30f26eaf
David Dietrich <daviddietrich@ddietrich-lt.cs.uwaterloo.ca>
2012-11-23 00:47:39 -0500

    small script

---
+++ Scripts/updateCassandraJars.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# Will update the cassandra jars into the cassandra source code directory
+
+`cp apache-cassandra-1.1.6-SNAPSHOT.jar /opt/apache-cassandra-1.1.6/lib/apache-cassandra-1.1.6.jar`
+`cp apache-cassandra-clientutil-1.1.6-SNAPSHOT.jar /opt/apache-cassandra-1.1.6/lib/apache-cassandra-clientutil-1.1.6.jar`
+`cp apache-cassandra-thrift-1.1.6-SNAPSHOT.jar /opt/apache-cassandra-1.1.6/lib/apache-cassandra-thrift-1.1.6.jar`
